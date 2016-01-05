8c7a34efd2cd42298bf8b736fa53eb6a0a25f44a
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2016-01-04 21:01:32 -0800

    moved directives to component folder, cleaned up MainController using ES6 Classes

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -7,7 +7,7 @@ angular.module('bApp')
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
-        controllerAs: 'main'
+        controllerAs: 'MainController'
       })
       .state('campaignProfile', {
         url: '/campaignProfile/:id',
